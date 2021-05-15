2df3becb08f94255a0b4fad5f55d6e2b5e5f93a4
lee21h <92387808+lee21h@users.noreply.github.com>
2021-05-15 21:04:55 +0000

    folder restructure

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -6,4 +6,5 @@ div
       p
         strong OLOTC
         |  | View code on
-        a(href='https://github.com/s-jannson/OLOTC', 
+        a(href='https://github.com/s-jannson/OLOTC', target="_blank")  Github
+        |  (and do feel free to improve and open PRs)
