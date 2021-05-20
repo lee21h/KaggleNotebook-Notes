f3c63ed8b17e9b030e92645ba3780e737c821cfc
lee21h <92387808+lee21h@users.noreply.github.com>
2021-05-20 11:56:09 +0000

    fix #124

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -20,3 +20,38 @@ export default {
   styleResources: {
     scss: ['@/assets/scss/style.scss']
   },
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [{ src: '@/plugins/echarts.js' }, { src: '@/plugins/vue-spinners.js' }],
+
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+    '@nuxtjs/composition-api/module',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/buefy
+    'nuxt-buefy',
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: process.env.BASE_URL || 'http://localhost:5000',
+    proxy: false,
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    transpile: [/echarts/, /zrender/]
+  },
+}
