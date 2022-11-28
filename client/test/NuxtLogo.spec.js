3bb9851a85a14abac3cac75be9e7039a9b9dfd94
lee21h <92387808+lee21h@users.noreply.github.com>
2022-11-28 07:14:16 +0000

    09

--- client/test/NuxtLogo.spec.js
+++ client/test/NuxtLogo.spec.js
@@ -3,4 +3,7 @@ import NuxtLogo from '@/components/NuxtLogo.vue'
 
 describe('NuxtLogo', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount
+    const wrapper = mount(NuxtLogo)
+    expect(wrapper.vm).toBeTruthy()
+  })
+})
