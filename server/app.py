65b5b949532b81284368d4491ec1452129265d3f
lee21h <92387808+lee21h@users.noreply.github.com>
2023-02-11 22:50:30 +0000

    small error

--- server/app.py
+++ server/app.py
@@ -2,4 +2,7 @@ from datetime import datetime
 from functools import total_ordering
 
 from flask import Flask, request, Response, jsonify
-from flask_cors import CORS, cross_o
+from flask_cors import CORS, cross_origin
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name_
