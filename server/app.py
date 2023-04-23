3d75a83ec9790d122e8a94748ee5504455634596
lee21h <92387808+lee21h@users.noreply.github.com>
2023-04-23 11:05:07 +0000

    Update 0300-nvidia.md

--- server/app.py
+++ server/app.py
@@ -57,4 +57,5 @@ def new_otc_trade():
                 status=400,
             )
         _datetime = datetime.strptime(data['datetime'], '%Y-%m-%dT%H:%M:%S.%fZ')
-        total_value = float(data['price']) * float(data['quantit
+        total_value = float(data['price']) * float(data['quantity'])
+        new_trade = trade(datetime=_datetime, price_usd=data['price'], quantit
